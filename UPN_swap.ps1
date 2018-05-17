695b46044bde1e1605aac03d1ef3cb53bb58180d
Matthew J. Greenlaw <mgreenlaw@cetechno.com>
2018-05-17 12:30:21 -0400

    Fixed string split

--- UPN_swap.ps1
+++ UPN_swap.ps1
@@ -7,15 +7,15 @@ param (
 )
 
 If ($ChangeFrom -ne "") {
-    $ADUsers = Get-ADUser -Filter {UserPrincipalName -like $ChangeFrom} -Properties userPrincipalName -ResultSetSize $null
+    $ADUsers = Get-ADUser -Filter {UserPrincipalName -like $ChangeFrom } -Properties userPrincipalName -ResultSetSize $null
 }
 Else {
-    $ADUsers = Get-ADUser-Properties userPrincipalName -ResultSetSize $null
+    $ADUsers = Get-ADUser -Filter 'UserPrincipalName -like "*"'  -Properties userPrincipalName -ResultSetSize $null
 }
 
 If ($ADUsers -ne $null) {
     foreach ($User in $ADusers) {
-        $newUpn = $User.UserPrincipalName.Replace("contoso.local", $ChangeTo)
+        $newUpn = $User.UserPrincipalName.Replace( (($User.UserPrincipalName.Split("@"))[1]) , $ChangeTo)
         If (!($dry)) {
             #Set-ADUser -UserPrincipalName $newUpn
         } 
