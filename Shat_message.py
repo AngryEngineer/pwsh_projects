3349a3465ffa08697aa9293b53b1d90e0e39ef4d
Matthew J. Greenlaw <mgreenlaw@cetechno.com>
2018-08-28 12:02:02 -0400

    bug

--- Shat_message.py
+++ Shat_message.py
@@ -7,7 +7,7 @@ from json import dumps
 def main():
     url = 'https://chat.googleapis.com/v1/spaces/AAAA0yYN5pI/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=xlzpEguXD5smrp4G3C6QHJvmobPCGfWkyIUWvPNJAYk%3D'
     bot_message = {
-        'text' : '<users/all> *Matthew* is *unable* to awnser the phone!!!'}
+        'text' : '<users/all> *Matthew* is *unable* to awnser the phone!!'}
 
     message_headers = { 'Content-Type': 'application/json; charset=UTF-8'}
 
