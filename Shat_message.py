bf165fa31f5df0bccb5c5dbbc86f58619fa3fa08
AngryEngineer <37904572+AngryEngineer@users.noreply.github.com>
2018-08-28 12:20:07 -0400

    Update Shat_message.py

--- Shat_message.py
+++ Shat_message.py
@@ -5,7 +5,7 @@ from json import dumps
 # Hangouts Chat incoming webhook quickstart
 #
 def main():
-    url = 'https://chat.googleapis.com/v1/spaces/AAAA0yYN5pI/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=xlzpEguXD5smrp4G3C6QHJvmobPCGfWkyIUWvPNJAYk%3D'
+    url = ''
     bot_message = {
         'text' : '<users/all> *Matthew* is *unable* to awnser the phone!!'}
 
